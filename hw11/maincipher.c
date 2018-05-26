f82cc04fef01c5e6e17975c03e25b1db8e6efb2f
Christina <34012766+cbraga909@users.noreply.github.com>
2018-05-25 17:56:34 -0700

    Create maincipher.c

---
+++ hw11/maincipher.c
@@ -0,0 +1,13 @@
+//Day 11 Homework: Cipher
+//This program shifts a message by a user-specified number of places
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "cipher.h"
+
+int main()
+{
+  EncryptMessage(); //Call message encryption function
+  return 0;
+}
